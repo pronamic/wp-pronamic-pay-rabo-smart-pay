708f2340aabf78eec7c478e2ba2f190b900d4cae
Remco Tolsma <info@remcotolsma.nl>
2018-08-23 12:22:44 +0200

    Added Address and CustomerInformation class.

---
+++ src/Address.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Address
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2018 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Gateways\OmniKassa2
+ */
+
+namespace Pronamic\WordPress\Pay\Gateways\OmniKassa2;
+
+use InvalidArgumentException;
+use stdClass;
+use JsonSchema\Constraints\Constraint;
+use JsonSchema\Exception\ValidationException;
+use JsonSchema\Validator;
+
+/**
+ * Address
+ *
+ * @author  Remco Tolsma
+ * @version 2.0.0
+ * @since   1.0.0
+ */
+class Address {
+	/**
+	 * First name.
+	 *
+	 * @var string
+	 */
+	private $first_name;
+
+	/**
+	 * Insert or second name.
+	 *
+	 * @var string
+	 */
+	private $middle_name;
+
+	/**
+	 * Surname.
+	 *
+	 * @var string
+	 */
+	private $last_name;
+
+	/**
+	 * Street.
+	 *
+	 * Note: In case of payment via Visa, Mastercard, V PAY,
+	 * Bancontact and Maestro the street name will be 
+	 * truncated to 50 characters.
+	 *
+	 * @var string
+	 */
+	private $street;
+
+	/**
+	 * House number.
+	 *
+	 * Note: In case of payment via Visa, Mastercard, V PAY, 
+	 * Bancontact and Maestro the houseNumber concatenated
+	 * with houseNumberAddition (see below) will be 
+	 * truncated to 10 characters.
+	 *
+	 * @var string
+	 */
+	private $house_number;
+
+	/**
+	 * House number additions.
+	 *
+	 * @var string
+	 */
+	private $house_number_addition;
+
+	/**
+	 * Postal code.
+	 *
+	 * @var string
+	 */
+	private $postal_code;
+
+	/**
+	 * City.
+	 *
+	 * @var string
+	 */
+	private $city;
+
+	/**
+	 * Country code, ISO 3166-1 alpha-2.
+	 *
+	 * @var string
+	 */
+	private $country_code;
+
+	/**
+	 * Get JSON.
+	 *
+	 * @return object
+	 */
+	public function get_json() {
+		$data = array(
+			'firstName'           => $this->first_name,
+			'middleName'          => $this->middle_name,
+			'lastName'            => $this->last_name,
+			'street'              => $this->street,
+			'houseNumber'         => $this->house_number,
+			'houseNumberAddition' => $this->house_number_addition,
+			'postalCode'          => $this->postal_code,
+			'city'                => $this->city,
+			'countryCode'         => $this->country_code,
+		);
+
+		$data = array_filter( $data );
+
+		if ( empty( $data ) ) {
+			return null;
+		}
+
+		return (object) $data;
+	}
+}
---
+++ src/CustomerInformation.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Customer information
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2018 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Gateways\OmniKassa2
+ */
+
+namespace Pronamic\WordPress\Pay\Gateways\OmniKassa2;
+
+use InvalidArgumentException;
+use stdClass;
+use JsonSchema\Constraints\Constraint;
+use JsonSchema\Exception\ValidationException;
+use JsonSchema\Validator;
+
+/**
+ * Customer information
+ *
+ * @author  Remco Tolsma
+ * @version 2.0.0
+ * @since   1.0.0
+ */
+class CustomerInformation {
+	/**
+	 * The e-mailadress of the consumer.
+	 *
+	 * @var string
+	 */
+	private $email_address;
+
+	/**
+	 * The date of birth of the consumer.
+	 *
+	 * @var string
+	 */
+	private $date_of_birth;
+
+	/**
+	 * The gender of the consumer.
+	 *
+	 * @var string
+	 */
+	private $gender;
+
+	/**
+	 * The initials of the consumer.
+	 *
+	 * @var string
+	 */
+	private $initials;
+
+	/**
+	 * The consumer's telephone number.
+	 *
+	 * @var string
+	 */
+	private $telephone_number;
+
+	/**
+	 * Get JSON.
+	 *
+	 * @return object
+	 */
+	public function get_json() {
+		$data = array(
+			'emailAddress'    => $this->email_address,
+			'dateOfBirth'     => $this->date_of_birth,
+			'gender'          => $this->gender,
+			'initials'        => $this->initials,
+			'telephoneNumber' => $this->telephoneNumber,
+		);
+
+		$data = array_filter( $data );
+
+		if ( empty( $data ) ) {
+			return null;
+		}
+
+		return (object) $data;
+	}
+}
