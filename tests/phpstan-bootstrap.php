0795a536406928aa4380a5aa793fa376b92b347b
Reüel van der Steege <reuel@pronamic.nl>
2018-09-26 13:08:29 +0200

    Update incorrect package name in tests file header.

--- tests/phpstan-bootstrap.php
+++ tests/phpstan-bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Bootstrap PHPStan
+ * Bootstrap PHPStan.
  *
  * @author    Pronamic <info@pronamic.eu>
  * @copyright 2005-2018 Pronamic
  * @license   GPL-3.0-or-later
- * @package   Pronamic\WordPress\Pay\Extensions\MemberPress
+ * @package   Pronamic\WordPress\Pay\Gateways\OmniKassa2
  */
 
 putenv( 'WP_PHPUNIT__TESTS_CONFIG=tests/wp-config.php' );
