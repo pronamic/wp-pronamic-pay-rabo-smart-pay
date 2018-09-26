5450580935a7eafaf7eb57a23d7899d4f3f87973
Reüel van der Steege <reuel@pronamic.nl>
2018-09-26 13:47:03 +0200

    Add product categories constants.

---
+++ src/ProductCategories.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Product categories.
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2018 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Gateways\OmniKassa2
+ */
+
+namespace Pronamic\WordPress\Pay\Gateways\OmniKassa2;
+
+/**
+ * Product categories.
+ *
+ * @author  Reüel van der Steege
+ * @version 2.0.4
+ * @since   2.0.4
+ */
+class ProductCategories {
+	/**
+	 * Product category 'PHYSICAL'.
+	 *
+	 * @var string
+	 */
+	const PHYSICAL = 'PHYSICAL';
+
+	/**
+	 * Product category 'DIGITAL'.
+	 *
+	 * @var string
+	 */
+	const DIGITAL = 'DIGITAL';
+}
