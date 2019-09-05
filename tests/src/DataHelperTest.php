14eba7b7bb2ba0002ddcb591841062dab695bfde
Remco Tolsma <info@remcotolsma.nl>
2019-09-05 11:44:03 +0200

    Added test for `validate_an`, `wp_strip_all_tags` and `trim` issue.

--- tests/src/DataHelperTest.php
+++ tests/src/DataHelperTest.php
@@ -20,13 +20,46 @@ namespace Pronamic\WordPress\Pay\Gateways\OmniKassa2;
 class DataHelperTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 * Test validate AN.
+	 *
+	 * @dataProvider validate_an_provider
+	 *
+	 * @param string $string   String.
+	 * @param int    $length   Length.
+	 * @param bool   $expected Expected.
 	 */
-	public function test_validate_an() {
-		$value = '1234567890';
+	public function test_validate_an( $string, $length, $exception = null ) {
+		if ( null !== $exception ) {
+			$this->expectException( $exception );
+		}
 
-		$result = DataHelper::validate_an( $value, 10 );
+		$result = DataHelper::validate_an( $string, $length );
 
-		$this->assertTrue( $result );
+		if ( null === $exception ) {
+			$this->assertTrue( $result );
+		}
+	}
+
+	/**
+	 * Validate AN provider.
+	 *
+	 * @return array
+	 */
+	public function validate_an_provider() {
+		return array(
+			// Valid.
+			array( '1234567890', 50 ),
+			/**
+			 * In `DataHelper::validate_an` we use `wp_strip_all_tags` which also uses `trim`:
+			 *
+			 * @link https://github.com/WordPress/WordPress/blob/5.2/wp-includes/formatting.php#L5070-L5092
+			 */
+			array( ' 1234567890', 50 ),
+			array( '1234567890 ', 50 ),
+			array( ' 1234567890 ', 50 ),
+			// Invalid.
+			array( '<p>1234567890</p>', 50, \InvalidArgumentException::class ),
+			array( '<script>1234567890</script>', 50, \InvalidArgumentException::class ),
+		);
 	}
 
 	/**
