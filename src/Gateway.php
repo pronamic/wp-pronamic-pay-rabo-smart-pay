719527765094dca94a3a0a0c86ea92dc2cf1060d
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-07-13 16:11:48 +0200

    Clarify iDEAL issuers method.

--- src/Gateway.php
+++ src/Gateway.php
@@ -70,7 +70,7 @@ class Gateway extends Core_Gateway {
 		$ideal_issuer_field = new SelectField( 'ideal-issuer' );
 
 		$ideal_issuer_field->set_options_callback( function() {
-			return $this->get_issuers();
+			return $this->get_ideal_issuers();
 		} );
 
 		$ideal_payment_method->add_field( $ideal_issuer_field );
@@ -88,11 +88,11 @@ class Gateway extends Core_Gateway {
 	}
 
 	/**
-	 * Get issuers.
+	 * Get iDEAL issuers.
 	 *
 	 * @return array<int, array<string, array<string>>>
 	 */
-	private function get_issuers() {
+	private function get_ideal_issuers() {
 		$groups = [];
 
 		// Maybe update access token.
