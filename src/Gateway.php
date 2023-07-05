pronamic/wp-pronamic-pay-rabo-smart-pay

commit 6fe8f00c6c27e5cd29fbf309fd24fb8908ede03f

Update for removed payment ID fallback in formatted payment string (pronamic/wp-pronamic-pay-adyen#23).

src/Gateway.php
@@ -255,6 +255,10 @@ class Gateway extends Core_Gateway {
 		// Merchant order ID.
 		$merchant_order_id = $payment->format_string( $this->config->order_id );
 
+		if ( '' === $merchant_order_id ) {
+			$merchant_order_id = $payment->get_id();
+		}
+
 		$payment->set_meta( 'omnikassa_2_merchant_order_id', $merchant_order_id );
 
 		// New order.

commit ff70fd97a3e37f8cbc2dde128d18126ce369ad76

Fix error "Field `Order.merchantOrderId` cannot contain HTML tags: `123`" (regression by 6fe8f00).

src/Gateway.php
@@ -256,7 +256,7 @@ class Gateway extends Core_Gateway {
 		$merchant_order_id = $payment->format_string( $this->config->order_id );
 
 		if ( '' === $merchant_order_id ) {
-			$merchant_order_id = $payment->get_id();
+			$merchant_order_id = (string) $payment->get_id();
 		}
 
 		$payment->set_meta( 'omnikassa_2_merchant_order_id', $merchant_order_id );