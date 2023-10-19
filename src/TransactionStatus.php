0223045ae6a347e5113b6966b537b50291b1c720
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-10-19 19:43:55 +0200

    Create TransactionStatus.php

---
+++ src/TransactionStatus.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Transaction status
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2023 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Gateways\OmniKassa2
+ */
+
+namespace Pronamic\WordPress\Pay\Gateways\OmniKassa2;
+
+use Pronamic\WordPress\Pay\Payments\PaymentStatus;
+
+/**
+ * Transaction status class
+ */
+class TransactionStatus {
+	/**
+	 * Success.
+	 *
+	 * @var string
+	 */
+	const SUCCESS = 'SUCCESS';
+
+	/**
+	 * Accepted.
+	 *
+	 * @var string
+	 */
+	const ACCEPTED = 'ACCEPTED';
+
+	/**
+	 * Cancelled.
+	 *
+	 * @var string
+	 */
+	const CANCELLED = 'CANCELLED';
+
+	/**
+	 * Expired.
+	 *
+	 * @var string
+	 */
+	const EXPIRED = 'EXPIRED';
+
+	/**
+	 * Failure.
+	 *
+	 * @var string
+	 */
+	const FAILURE = 'FAILURE';
+}
